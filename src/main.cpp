f85df31b42e7a01f180904b0e6b646c9b32e90e0
Akingram <guilland.killian@gmail.com>
2025-05-20 09:42:14 +0200

    Working opengl setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,165 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+#include "glad/glad.h"
+#include "draw_scene.hpp"
+#include "tools/matrix_stack.hpp"
 #include <iostream>
-#include <fstream>
-#include <sstream>
+#include <cmath>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#include "graphics/sprite.hpp"
 
-std::string loadFile(const std::string& path) {
-    std::ifstream file(path);
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
+/* Window size */
+
+int WINDOW_WIDTH = 800;
+int WINDOW_HEIGHT = 800;
+
+using namespace glbasimac;
+
+/* Minimal time wanted between two images */
+static const double FRAMERATE_IN_SECONDS = 1. / 30.;
+static float aspectRatio = 1.0f;
+
+/* Error handling function */
+void onError(int error, const char *description)
+{
+    std::cout << "GLFW Error (" << error << ") : " << description << std::endl;
 }
 
-GLuint compileShader(GLenum type, const std::string& source) {
-    GLuint shader = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(shader, 1, &src, nullptr);
-    glCompileShader(shader);
-    return shader;
+static const float GL_VIEW_SIZE = 6.0f;
+
+void onWindowResized(GLFWwindow *, int width, int height)
+{
+    aspectRatio = width / (float)height;
+    myEngine.set3DProjection(90.0, aspectRatio, Z_NEAR, Z_FAR);
+};
+
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS || action == GLFW_REPEAT)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GL_TRUE);
+            break;
+        case GLFW_KEY_W:
+            angle_phy += 10.0f;
+            break;
+        case GLFW_KEY_S:
+            angle_phy -= 10.0f;
+            break;
+        case GLFW_KEY_A:
+            angle_theta -= 10.0f;
+            break;
+        case GLFW_KEY_D:
+            angle_theta += 10.0f;
+            break;
+        case GLFW_KEY_UP:
+            dist_zoom *=0.6f;
+            break;
+        case GLFW_KEY_DOWN:
+            dist_zoom *= 1.5f;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
-GLuint createShaderProgram(const std::string& vsPath, const std::string& fsPath) {
-    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, loadFile(vsPath));
-    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, loadFile(fsPath));
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertexShader);
-    glAttachShader(program, fragmentShader);
-    glLinkProgram(program);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    return program;
-}
+int main()
+{
+    // Set error callback
+    glfwSetErrorCallback( []( int, const char* desc )
+    {
+        std::cerr << "GLFW Error: " << desc << "\n";
+        std::exit( EXIT_FAILURE );
+    } );
 
-int main() {
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Simple Sprite", nullptr, nullptr);
+    // Initialize the library
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    /* Callback to a function if an error is rised by GLFW */
+    glfwSetErrorCallback(onError);
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGLTemplate", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // -- Callbacks --
+    glfwSetWindowSizeCallback(window, onWindowResized);
+
+    // Make the window's context current
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    GLuint shader = createShaderProgram("sprite.vs", "sprite.fs");
-    glUseProgram(shader);
-    glUniform1i(glGetUniformLocation(shader, "sprite"), 0); // bind texture unit 0
+    // Intialize glad (loads the OpenGL functions)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        return -1;
+    }
 
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-    Sprite mySprite("assets/images/sprite.png", 32, 32);
-    std::cout << "Sprite created" << std::endl;
+    // Initialize Rendering Engine
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
+    myEngine.mode2D = false;
 
-        mySprite.draw(200.0f, 150.0f); // draw at x=200, y=150
+    myEngine.initGL();
 
+    onWindowResized(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    initScene();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Get time (in second) at loop beginning */
+        double startTime = glfwGetTime();
+
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+
+        /* Fix camera position */
+        myEngine.mvMatrixStack.loadIdentity();
+        Vector3D pos_camera = Vector3D(dist_zoom * cos(deg2rad(angle_theta)) * cos(deg2rad(angle_phy)),
+                                       dist_zoom * sin(deg2rad(angle_theta)) * cos(deg2rad(angle_phy)),
+                                       dist_zoom * sin(deg2rad(angle_phy)));
+
+        Vector3D viewed_point = Vector3D(0.0f, 0.0f, 0.0f);
+        Vector3D up_vector = Vector3D(0.0f, 0.0f, 1.0f);
+        Matrix4D view_matrix = Matrix4D::lookAt(pos_camera, viewed_point, up_vector);
+        myEngine.setViewMatrix(view_matrix);
+        myEngine.updateMvMatrix();
+
+        drawScene();
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
+
+        /* Elapsed time computation from loop begining */
+        double elapsedTime = glfwGetTime() - startTime;
+        /* If to few time is spend vs our wanted FPS, we wait */
+        while (elapsedTime < FRAMERATE_IN_SECONDS)
+        {
+            glfwWaitEventsTimeout(FRAMERATE_IN_SECONDS - elapsedTime);
+            elapsedTime = glfwGetTime() - startTime;
+        }
     }
 
     glfwTerminate();
--- src/shaders/sprite.fs
+++
@@ -1,9 +0,0 @@
-#version 410 core
-out vec4 FragColor;
-
-in vec2 TexCoord;
-uniform sampler2D sprite;
-
-void main() {
-    FragColor = texture(sprite, TexCoord);
-}
