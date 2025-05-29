dd3a91c8647e8a0f1d3685ba2a68eb12df38494e
BQuent1 <quentin.baud9@gmail.com>
2025-05-29 18:48:52 +0200

    crée un tableau avec chaque sprite

--- src/draw_scene.cpp
+++ src/draw_scene.cpp
@@ -2,18 +2,23 @@
 #include <tuple>
 #include <stb_image.h>
 #include "graphics/sprite.hpp"
+#include <iomanip>
+#include <sstream>
 
 GLBI_Engine myEngine;
 
 GLBI_Set_Of_Points originPoint;
 
-Sprite* tile000 {};
-Sprite* tile001 {};
+std::vector<Sprite *> tileset;
 
 void initScene()
 {
-	tile000 = new Sprite("tileset/tile_000.png", 0.2f, 0.2f);
-	tile001 = new Sprite("tileset/tile_020.png", 0.2f, 0.2f);
+	for (int i = 0; i < 114; ++i)
+	{
+		std::ostringstream ss;
+		ss << "tileset/tile_" << std::setw(3) << std::setfill('0') << i << ".png";
+		tileset.push_back(new Sprite(ss.str(), 0.2f, 0.2f));
+	}
 
 	std::vector<float> origin = {0.0f, 0.0f};
 	std::vector<float> originColor = {1.0f, 0.0f, 0.0f};
@@ -23,9 +28,9 @@ void initScene()
 
 void drawScene()
 {
-	glClearColor(0.5f,0.5f,0.5f,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glEnable(GL_DEPTH_TEST);
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
 
 	originPoint.drawSet();
 
@@ -34,16 +39,16 @@ void drawScene()
 	glPointSize(10.0f);
 	myEngine.setFlatColor(1.0f, 1.0f, 1.0f);
 	glLineWidth(2.0f);
-	
+
 	// draw quad
 	myEngine.mvMatrixStack.pushMatrix();
-		myEngine.mvMatrixStack.addTranslation(Vector3D(0.0f, 0.0f, 0.0f));
-		myEngine.updateMvMatrix();
-		tile000->draw();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0.0f, 0.0f, 0.0f));
+	myEngine.updateMvMatrix();
+	tileset[0]->draw();
 	myEngine.mvMatrixStack.popMatrix();
 	myEngine.mvMatrixStack.pushMatrix();
-		myEngine.mvMatrixStack.addTranslation(Vector3D(0.2f, 0.2f, 0.2f));
-		myEngine.updateMvMatrix();
-		tile001->draw();
+	myEngine.mvMatrixStack.addTranslation(Vector3D(0.2f, 0.2f, 0.2f));
+	myEngine.updateMvMatrix();
+	tileset[113]->draw();
 	myEngine.mvMatrixStack.popMatrix();
 }
